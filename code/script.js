fe4a7ae4a196a290acedee63b431a7d111e3cfe9
Aathi0001 <142761004+Aathi0001@users.noreply.github.com>
2025-08-03 23:33:18 +0530

    Add files via upload

---
+++ code/script.js
@@ -0,0 +1,59 @@
+//var
+a=10
+console.log(a)
+var a=10 //Declaration + Initialization
+console.log(a)
+var b //Declaration
+console.log(b)
+b=20 //initialization
+console.log(b)
+b=30 //re-initialization
+console.log(b)
+var b=40 //re-declaration
+console.log(b)
+
+// Output:
+// 10
+// 10
+// undefined
+// 20
+// 30
+// 40
+
+//let
+let c=10 //Initialization + Declaration
+console.log(c)
+let d //Declaration
+console.log(d)
+d=20 //initialization
+console.log(d)
+d=30 //re-initialization
+console.log(d)
+// let c = 100 //re-declaration       Not Possible
+// console.log(c)
+
+// Output:
+// 10
+// undefined
+// 20
+// 30
+
+//const
+const e=10 //Declaration + Initializaation
+console.log(e)
+// const b //declaration              Not Possible
+// console.log(b)
+// f=20 //Initialization              Not Possible
+// console.log(f)
+// e=30 //re-initiaization            Not Possible
+// console.log(e)
+// const e=40 //re-declaration        Not Possible
+// console.log(e)
+
+// Output:
+// 10
+
+console.log()
+console.log()
+console.log()
+console.log()
